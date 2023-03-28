83401c13c24add5c4407c50f7c5afca37f525e79
Sasank Tadepalli <sasank.tadepalli@icloud.com>
2023-03-28 16:50:03 -0700

    Started Assignment

--- Invitation System/src/main/webapp/FormHandler.js
+++ Invitation System/src/main/webapp/FormHandler.js
@@ -1,39 +1,57 @@
 /**
- * 
+ *
  */
 
- const formElement = document.getElementById("event-form")
- const eventNameElement = document.getElementById("name")
- const eventTimeElement = document.getElementById("time")
- const eventDateElement = document.getElementById("date")
- const eventLocationElement = document.getElementById("location")
- const eventDescriptionElement = document.getElementById("description")
- 
- formElement.addEventListener("submit", event => {
-	 event.preventDefault()
-	 console.log(typeof eventDateElement.value)
-	 console.log(eventDateElement.value)
-	 const eventDate = new Date(eventDateElement.value + " " + eventTimeElement.value)
-	 console.log(eventDate)
-	 const currentDate = new Date()
-	 console.log(currentDate)
-	 console.log(eventDate - currentDate)
-	 
-	 if(eventNameElement.value == "") alert("Please specify the Event Name")
-	 else if(eventDateElement.value == "") alert("Please select the Event Date")
-	 else if(eventTimeElement.value == "") alert("Please select the Event Time")
-	 else if(eventLocationElement.value == "") alert("Please specify the Event Location")
-	 else if(eventDescriptionElement.value == "") alert("Please describe about the Event in a few words")
-	 else{
-		 const eventDate = new Date(eventDateElement.value + " " + eventTimeElement.value)
-		 console.log(eventDate)
-		 const currentDate = new Date()
-		 console.log(currentDate)
-		 eventNameElement.value = ""
-		 eventDateElement.value = ""
-		 eventTimeElement.value = ""
-		 eventLocationElement.value = ""
-		 eventDescriptionElement.value = ""
-		 alert("Event has been successfully registered")
-	 }
- })
+const formElement = document.getElementById("event-form");
+const eventNameElement = document.getElementById("name");
+const eventTimeElement = document.getElementById("time");
+const eventDateElement = document.getElementById("date");
+const eventLocationElement = document.getElementById("location");
+const eventDescriptionElement = document.getElementById("description");
+const eventAttendElements = document.getElementsByName("attending");
+var eventAttendanceStatus = "";
+
+formElement.addEventListener("submit", (event) => {
+  event.preventDefault();
+  for (var i = 0; i < eventAttendElements.length; i++) {
+    if (eventAttendElements[i].checked) {
+      eventAttendanceStatus = eventAttendElements[i].value;
+    }
+  }
+  validateForm();
+});
+
+const validateForm = () => {
+  if (eventNameElement.value == "") alert("Please specify the Event Name");
+  else if (eventDateElement.value == "") alert("Please select the Event Date");
+  else if (eventTimeElement.value == "") alert("Please select the Event Time");
+  else if (eventLocationElement.value == "")
+    alert("Please specify the Event Location");
+  else if (eventDescriptionElement.value == "")
+    alert("Please describe about the Event in a few words");
+  else if (eventAttendanceStatus == "")
+    alert("Please select whether you want to attend the event or not");
+  else if (validateEventDateAndTime() < 0)
+    alert(
+      "Time and Date of the Event should be in Future Date and Time but not in Past"
+    );
+  else {
+    eventNameElement.value = "";
+    eventDateElement.value = "";
+    eventTimeElement.value = "";
+    eventLocationElement.value = "";
+    eventDescriptionElement.value = "";
+    for (var i = 0; i < eventAttendElements.length; i++) {
+      eventAttendElements[i].checked = false;
+    }
+    alert("Event has been successfully registered");
+  }
+};
+
+const validateEventDateAndTime = () => {
+  const eventDate = new Date(
+    eventDateElement.value + " " + eventTimeElement.value
+  );
+  const currentDate = new Date();
+  return eventDate - currentDate;
+};
