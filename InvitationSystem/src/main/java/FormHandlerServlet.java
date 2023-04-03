57816fc3142e82dd4d9a15f0e9fbaae7a28915c8
Sasank Tadepalli <sasank.tadepalli@icloud.com>
2023-04-02 17:38:45 -0700

    Completed Assignment 3

--- InvitationSystem/src/main/java/FormHandlerServlet.java
+++ InvitationSystem/src/main/java/FormHandlerServlet.java
@@ -30,7 +30,7 @@ public class FormHandlerServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.sendRedirect("FormHandler.html");
+		response.sendRedirect("EventForm.jsp");
 	}
 
 	/**
@@ -45,54 +45,14 @@ public class FormHandlerServlet extends HttpServlet {
 		String eventDescription = request.getParameter("event_description");
 		String eventAttendance = request.getParameter("attending");
 		
-		PrintWriter out = response.getWriter();
-		out.print("<!DOCTYPE html>\n"
-				+ "<html>\n"
-				+ "  <head>\n"
-				+ "    <meta charset=\"UTF-8\" />\n"
-				+ "    <title>Invitation System Project</title>\n"
-				+ "    <link rel=\"stylesheet\" href=\"./FormHandler.css\" />\n"
-				+ "  </head>\n"
-				+ "  <body>\n"
-				+ "    <h1>Event Details</h1>\n"
-				+ "    <div class=\"form-container\">\n"
-				+ "      <form action=\"FormHandlerServlet\" method=\"POST\" id=\"event-form\">\n"
-				+ "        <div>\n"
-				+ "          <label>Event Name:</label>"
-				+ "			 <input type=\"text\" name=\"event_name\" value=" + eventName + " id=\"eventDetails\" readonly />\n"	
-				+ "        </div>\n"
-				+ "        <div>\n"
-				+ "          <label>Event Date:</label>"
-				+ "			 <input type=\"date\" name=\"event_date\" value=" + eventDate + " id=\"eventDetails\" readonly />\n"
-				+ "        </div>\n"
-				+ "        <div>\n"
-				+ "          <label>Event Time:</label>"
-				+ "			 <input type=\"time\" name=\"event_time\" value=" + eventTime + " id=\"eventDetails\" readonly />\n"
-				+ "        </div>\n"
-				+ "        <div>\n"
-				+ "          <label>Event Location:</label>"
-				+ "			 <input type=\"text\" name=\"event_location\" value=" + eventLocation + " id=\"eventDetails\" readonly />\n"
-				+ "        </div>\n"
-				+ "        <div>\n"
-				+ "          <label>Event Description:</label>"
-				+ "			 <textarea name=\"event_description\" rows=\"4\" cols=\"10\" id=\"eventDetails\" readonly>" + eventDescription + "</textarea>\n"
-				+ "        </div>\n"
-				+ "\n"
-				);
+		request.setAttribute("event_name", eventName);
+		request.setAttribute("event_date", eventDate);
+		request.setAttribute("event_time", eventTime);
+		request.setAttribute("event_location", eventLocation);
+		request.setAttribute("event_description", eventDescription);
 		
 		if(eventAttendance == null) {
-			out.print(
-					"		   <div>\n"
-					+ "          <input type=\"submit\" value=\"Attend\" class=\"button\" id=\"attending\" name=\"attending\"/>\n"
-					+ "          <input type=\"submit\" value=\"Not Attend\" class=\"button\" id=\"not-attending\" name=\"attending\"/>\n"
-					+ "        </div>\n"
-					+ "      </form>\n"
-					+ "    </div>\n"
-					+ "\n"
-					+ "  </body>\n"
-					+ "</html>\n"
-					+ ""
-					);
+			request.getRequestDispatcher("EventAttendance.jsp").forward(request, response);
 		}
 		else {
 			if(eventAttendance.equals("Attend")) {
@@ -101,19 +61,10 @@ public class FormHandlerServlet extends HttpServlet {
 			else if(eventAttendance.equals("Not Attend")) {
 				numberOfPeopleNotAttending += 1;
 			}
-			out.print("</form>\n"
-					+ "<h4 id=\"h4\"> Number of People Attending: <span>" + numberOfPeopleAttending + "</span></h4>"
-					+ "<h4> Number of People Not Attending: <span>" + numberOfPeopleNotAttending + "</span></h4>"
-					+ "</div>\n"
-					+ "\n"
-					+ "</body>\n"
-					+ "</html>\n"
-					+ ""
-					);
+			request.setAttribute("attending", numberOfPeopleAttending);
+			request.setAttribute("not-attending", numberOfPeopleNotAttending);
+			request.getRequestDispatcher("EventConfirmation.jsp").forward(request, response);
 		}
-		
-		out.close();
-		out.flush();
 	}
 
 }
---
+++ InvitationSystem/src/main/webapp/EventForm.jsp
@@ -0,0 +1,43 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8" />
+    <title>Invitation System Project</title>
+    <link rel="stylesheet" href="./FormHandler.css" />
+  </head>
+  <body>
+    <h1>Invitation System</h1>
+    <div class="form-container">
+      <form action="FormHandlerServlet" method="POST" id="event-form">
+        <div>
+          <label>Event Name:</label>
+          <input type="text" id="name" name="event_name" value="" />
+        </div>
+        <div>
+          <label>Event Date:</label>
+          <input type="date" id="date" name="event_date" value="" />
+        </div>
+        <div>
+          <label>Event Time:</label>
+          <input type="time" id="time" name="event_time" value="" />
+        </div>
+        <div>
+          <label>Event Location:</label>
+          <input type="text" id="location" name="event_location" value="" />
+        </div>
+        <div>
+          <label>Event Description:</label>
+          <textarea id="description" name="event_description" rows="4" cols="10"></textarea>
+        </div>
+
+        <div class="button-container">
+          <input type="submit" value="Submit" class="button" />
+        </div>
+      </form>
+    </div>
+
+    <script src="./FormHandler.js"></script>
+  </body>
+</html>
